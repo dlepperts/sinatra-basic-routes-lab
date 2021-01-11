cad220e8fafd2df5e3e19f6fff89f568993e7840
dlepperts <danielle.leppertsimenauer@gmail.com>
2021-01-11 17:47:46 -0600

    commit tree 096b1e5c0a2250660f4208aa42325fd2a46d664c

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Danielle"
+    end 
+
+    get '/hometown' do
+        "My hometown is Mariposa, CA"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Tuesday's Gone"
+    end
+
 end
